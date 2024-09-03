ccd389d57d7f9a9f3b576e7f22f54fd38f7544ff
Eze-Ferrero <ezequielferreroa@gmail.com>
2024-09-03 19:35:13 -0300

    Faciles
    Ejercicios Faciles, practica parcial

---
+++ EliudsEggs/EliudsEggs/Ejercicio.cs
@@ -0,0 +1,72 @@
+﻿namespace EliudsEggs
+{
+
+    /*
+    Consigna:
+        Tu amigo Eliud heredó una granja de su abuela Tigist. Su abuela era inventora y tenía tendencia a construir cosas de una manera demasiado complicada. El gallinero tiene una pantalla digital que muestra un número codificado que representa las posiciones de todos los huevos que se pueden recoger.
+
+        Eliud te pide que escribas un programa que muestre el número real de huevos en el gallinero.
+
+        La codificación de la información de posición se calcula de la siguiente manera:
+
+        Escanee los lugares potenciales para poner huevos y marque un 1para un huevo existente o un 0para un lugar vacío.
+        Convierte el número de binario a decimal.
+        Mostrar el resultado en la pantalla.
+
+        Ejemplo 1:
+
+            Chicken Coop:
+             _ _ _ _ _ _ _
+            |E| |E|E| | |E|
+
+            Resulting Binary:
+             1 0 1 1 0 0 1
+
+            Decimal number on the display:
+            89
+
+            Actual eggs in the coop:
+            4
+        Ejemplo 2:
+
+            Chicken Coop:
+             _ _ _ _ _ _ _ _
+            | | | |E| | | | |
+
+            Resulting Binary:
+             0 0 0 1 0 0 0 0
+
+            Decimal number on the display:
+            16
+
+            Actual eggs in the coop:
+            1
+
+        Instrucciones
+        Su tarea es contar la cantidad de bits 1 en la representación binaria de un número.
+
+        Restricciones
+        ¡No utilices la función de recuento de bits que ofrece tu biblioteca estándar! Resuelve este problema tú mismo utilizando otras herramientas básicas.
+    */
+
+    public static class EliudsEggs
+    {
+        public static int EggCount(int encodedCount)
+        {
+            string binario = Convert.ToString(encodedCount, 2);
+
+
+            int count = 0;
+            foreach (char bit in binario)
+            {
+                if (bit == '1')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+
+}
---
+++ Isograma/Isograma/Ejercicio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isograma
+{
+    /*
+        Consigna: 
+            Determina si una palabra o frase es un isograma.
+
+            Un isograma (también conocido como "palabra sin patrón") es una palabra o frase sin una letra repetida; sin embargo, se permite que aparezcan espacios y guiones varias veces.
+
+            Ejemplos de isogramas:
+
+            lumberjacks
+            background
+            downstream
+            six-year-old
+
+            La palabra isogramas, sin embargo, no es un isograma porque la s se repite.
+
+            TIPS: Funciones ToCharArray, IndexOf y LastIndexOf de la clase String
+    */
+
+    public static class Isogram
+    {
+        public static bool IsIsogram(string word)
+        {
+            string palabra = word.ToLower().Replace(" ", "").Replace("-", "");
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if (palabra.IndexOf(palabra[i]) != palabra.LastIndexOf(palabra[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+}
